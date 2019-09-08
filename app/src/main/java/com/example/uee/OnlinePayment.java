55a90417e70a17ee1d4a34efbac85a1238b92f72
Chathurindu <100wpck@gmail.com>
2019-09-08 08:19:23 +0530

    login, register, pofile, profile update, payment interfaces

---
+++ app/src/main/java/com/example/uee/OnlinePayment.java
@@ -0,0 +1,15 @@
+package com.example.uee;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class OnlinePayment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_online_payment );
+
+    }
+}
---
+++ app/src/main/java/com/example/uee/editProfile.java
@@ -0,0 +1,34 @@
+package com.example.uee;
+
+import android.content.pm.ActivityInfo;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class editProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_edit_profile2 );
+        setRequestedOrientation ( ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
+    }
+}
